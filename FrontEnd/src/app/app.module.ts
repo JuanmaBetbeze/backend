6b781d1be27f304c1ce172b21faf90001f077f7d
JuanmaBetbeze <juanma.betbeze@gmail.com>
2022-08-21 18:58:59 -0300

    a

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListarComponent } from './Usuario/listar/listar.component';
+import { AddComponent } from './Usuario/add/add.component';
+import { EditComponent } from './Usuario/edit/edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListarComponent,
+    AddComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
