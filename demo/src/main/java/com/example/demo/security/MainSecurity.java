2e270db3ec69915d81ef817303dd8d87bc2b32a6
JuanmaBetbeze <juanma.betbeze@gmail.com>
2022-10-04 23:50:18 -0300

    try debug

--- demo/src/main/java/com/example/demo/security/MainSecurity.java
+++ demo/src/main/java/com/example/demo/security/MainSecurity.java
@@ -56,7 +56,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll().anyRequest().authenticated()
+                .antMatchers("/**").permitAll().anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
                 .and()
